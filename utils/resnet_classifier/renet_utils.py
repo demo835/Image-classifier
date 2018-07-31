27982343e9b5b655c7d80bd08da3eaccd323cefb
be <dreamyouth@engineer.com>
2018-07-31 10:26:43 +0800

    update README.md and setup.bash delete unused classifier

--- utils/resnet_classifier/renet_utils.py
+++
@@ -1,8 +0,0 @@
-
-class ResNetUtils:
-    def __init__(self):
-        pass
-
-    def get_feature_from_image(self, image_path):
-        pass
-
--- utils/resnet_classifier/resnet_settings.py
+++
@@ -1,8 +0,0 @@
-
-from src.settings import ROOT
-
-cur_dir = ROOT + "/utils/resnet_classifier"
-
-CLASSIFIER_DIR = cur_dir + "/classifier"
-FEATURES_DIR = cur_dir + "/features"
-IMGNET_DIR = cur_dir + "/resnet"
